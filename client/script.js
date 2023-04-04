238142297547f00014112eb2845cd1c9fd07f341
Gowtham Manohar <gowthammanohar1912@gmail.com>
2023-04-04 17:10:25 +0530

    Update server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,8 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://web-ai.onrender.com/', {
+    const response = await fetch('https://web-ai.onrender.com', 
+    {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
